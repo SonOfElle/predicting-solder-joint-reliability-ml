c37477eaf2c37dae360a9282c2bfbd4b0000c656
Ngonidzashe Ntuli <SonOfElle@users.noreply.github.com>
2026-09-17 16:09:29 +0200

    Scaffold repo structure, add Apache 2.0 license, resolve data licensing

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: data test clean
+
+data:
+	@echo "Regenerate synthetic dataset. Not yet implemented."
+
+test:
+	@echo "Run test suite. Not yet implemented."
+
+clean:
+	@echo "Remove generated artefacts. Not yet implemented."
---
+++ src/solder_reliability/__init__.py
@@ -0,0 +1,9 @@
+"""Solder joint reliability prediction.
+
+Refactored from the MSc thesis:
+Reliability prediction of soldering joints in electronic systems.
+Silesian University of Technology, 2023.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Ngonidzashe Ntuli"
